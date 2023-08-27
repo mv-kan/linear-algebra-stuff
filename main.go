4f1bb68a02ea82403b18df5fa1e23d6c67766210
michael kan <michael.kan.mail@gmail.com>
2023-08-27 18:43:20 +0200

    small refactor

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 type Scalar int
-type Vector []int
+
+/*
+VECTOR
+in linear algebra we have vectors not like that
+[1,2,3]
+but like that
+(vertically)
+
+	1
+	2
+	3
+*/
+type Vector [][]int
 type Matrix [][]int
 
+func CopySlice(s []int) []int {
+	copyS := make([]int, len(s))
+	copy(copyS, s)
+	return copyS
+}
+
 func CopyVector(v Vector) Vector {
-	copyV := make([]int, len(v))
-	copy(copyV, v)
+	copyV := make([][]int, len(v))
+	for i := 0; i < len(copyV); i++ {
+		copyV[i] = CopySlice(v[i])
+	}
 	return copyV
 }
 
 func CopyMatrix(m Matrix) Matrix {
 	copyM := make([][]int, len(m))
 	for i := 0; i < len(copyM); i++ {
-		copyM[i] = CopyVector(m[i])
+		copyM[i] = CopySlice(m[i])
 	}
 	return copyM
 }
 
 func main() {
-	// RunMultiplyingMatrices()
+	RunMultiplyingMatrices()
 	RunTranspose()
 }
